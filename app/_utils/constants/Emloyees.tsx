607cc9d90494be038d54b092db540a98ad99c1c3
Roman Rojas <rrojaski@gmail.com>
2024-11-02 15:41:01 -0400

    Update avatar URL for Roman Rojas in Employees constants

--- app/_utils/constants/Emloyees.tsx
+++ app/_utils/constants/Emloyees.tsx
@@ -8,6 +8,6 @@ const generateEmployee = (id: number, name: string, position: string, avatar: st
 };
 export const employees = [
   generateEmployee(3, "Romaura Rojas", "Project Manager", "https://images.webfluid.studio/romaura.jfif"),
-  generateEmployee(2, "Roman Rojas", "CTO & Lead Engineer", "https://images.webfluid.studio/roman.jpg"),
+  generateEmployee(2, "Roman Rojas", "CTO & Lead Engineer", "https://images.webfluid.studio/roman_3.JPG"),
   generateEmployee(1, "Marcio Rodrigues", "Software Engineer", "https://images.webfluid.studio/marcio.jfif"),
 ];
