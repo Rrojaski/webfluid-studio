3a26ef7941705586074fffcf5d310e7e15be0a51
Roman Rojas <rrojaski@gmail.com>
2024-11-27 16:23:02 -0500

    Refactor SanityCMS utility to streamline post structure and remove unused code

--- app/_utils/SanityCMS.tsx
+++ app/_utils/SanityCMS.tsx
@@ -4,38 +4,27 @@ import imageUrlBuilder from "@sanity/image-url";
 import { client } from "@/sanity/client";
 import { SanityDocument } from "next-sanity";
 
-const urlFor = (source: SanityImageSource) => (projectId && dataset ? imageUrlBuilder({ projectId, dataset }).image(source) : null);
 const { projectId, dataset } = client.config();
+const urlFor = (source: SanityImageSource) => (projectId && dataset ? imageUrlBuilder({ projectId, dataset }).image(source) : null);
 
 export const sanityToPost = (post: SanityDocument, largeImage = false): Post => {
-  const post2: Post = {
-    id: "",
-    title: "",
-    label: "",
-    image: "",
-    description: "",
-    body: "",
-    slug: "",
-    date: new Date(),
-    author: {
-      name: "",
-      title: "",
-      avatar: "",
-    },
-  };
-  return post2;
   const postImageUrl = post.image
     ? urlFor(post.image)
         ?.width(largeImage ? 1100 : 550)
         .url()
     : null;
 
-  const avatarImageUrl = post.author?.avatar ? urlFor(post.author.avatar)?.width(50).height(50).url() : null;
+  // const avatarImageUrl = post.author?.avatar ? urlFor(post.author.avatar)?.width(50).height(50).url() : null;
 
+  // const author = {
+  //   name: post.author?.name,
+  //   title: post.author?.title,
+  //   avatar: avatarImageUrl ?? "",
+  // };
   const author = {
-    name: post.author?.name,
-    title: post.author?.title,
-    avatar: avatarImageUrl ?? "",
+    name: "",
+    title: "",
+    avatar: "",
   };
 
   return {
