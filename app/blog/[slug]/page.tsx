4ce24f7f687833d774cbe706f93477e24206a142
Roman Rojas <rrojaski@gmail.com>
2024-11-27 15:41:07 -0500

    Add null check for slug parameter in BlogPost component

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,7 +3,9 @@ import "./page.css";
 import Box from "@mui/material/Box";
 
 async function BlogPost({ params }: { params: { slug: string } }) {
- 
+  if (!params.slug) return null;
+
+
   return (
     <Box>
    
