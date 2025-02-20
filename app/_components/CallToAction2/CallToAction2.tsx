0c0c4047e15e2d4b2564a826990bb0ca5c78072a
Roman Rojas <rrojaski@gmail.com>
2025-02-20 10:18:52 -0500

    Refactor CallToAction2 component styles and structure for improved readability and consistency

--- app/_components/CallToAction2/CallToAction2.tsx
+++ app/_components/CallToAction2/CallToAction2.tsx
@@ -7,40 +7,40 @@ function CallToAction2() {
   return (
     <Card className="call-to-action-card-2" variant="outlined">
       <CardContent className="call-to-action-wrapper">
-          <div className="call-to-action-container">
-            <section className="call-to-action-item">
-              <Typography
-                className="part-1"
-                variant="h5"
-                sx={{
-                  typography: { md: "h5", sm: "h5", sx: "h5" },
-                }}
-              >
-                {"Have a "}
-                <Typography component="span" sx={{ color:"#ED6C02", fontSize:"2rem", fontWeight:"bold"}}>
-                  {"{"}
-                </Typography>
-                {"project"}
-                <Typography component={"span"} sx={{ color:"#ED6C02", fontSize:"2rem", fontWeight:"bold" }}>
-                  {"}"}
-                </Typography>
-                {"in mind ?"}
+        <div className="call-to-action-container">
+          <section className="call-to-action-item">
+            <Typography
+              className="part-1"
+              variant="h5"
+              sx={{
+                typography: { md: "h5", sm: "h5", sx: "h5" },
+              }}
+            >
+              {"Have a "}
+              <Typography component="span" sx={{ color: "#ED6C02", fontSize: "2rem", fontWeight: "bold" }}>
+                {"{"}
               </Typography>
-            </section>
-            <div className="call-to-action-item">
-              <Typography variant="h6" className="call-to-action-sub" mb={1}>
-                Turn your ideas into innovative solutions with cutting-edge technology.
+              {"project"}
+              <Typography component={"span"} sx={{ color: "#ED6C02", fontSize: "2rem", fontWeight: "bold" }}>
+                {"}"}
               </Typography>
-            </div>
-            <div className="call-to-action-item">
-              <Link className="part-buttonaction" href="/contactUs">
-                <Button id="buttonaction" size="large" variant="contained">
-                  <p className="textbutton">Bring your ideas to life!</p>
-                </Button>
-              </Link>
-            </div>
+              {"in mind ?"}
+            </Typography>
+          </section>
+          <div className="call-to-action-item">
+            <Typography variant="h6" className="call-to-action-sub" mb={1}>
+              Turn your ideas into innovative solutions with cutting-edge technology.
+            </Typography>
           </div>
-  </CardContent>
+          <div className="call-to-action-item">
+            <Link className="part-buttonaction" href="/contactUs">
+              <Button id="buttonaction" size="large" variant="contained">
+                <p className="textbutton">Bring your ideas to life!</p>
+              </Button>
+            </Link>
+          </div>
+        </div>
+      </CardContent>
     </Card>
   );
 }
