5700e38d209697bfa74ae059dcb8cc0830137355
Roman Rojas <rrojaski@gmail.com>
2025-02-28 10:12:29 -0500

    Refactor OurProcess component styles and structure for improved layout and readability

--- app/_components/OurProcess/OurProcess.tsx
+++ app/_components/OurProcess/OurProcess.tsx
@@ -4,10 +4,20 @@ import { Box } from "@mui/system";
 
 function OurProcess() {
   return (
-    <Box className="our-process-container">
-      <Box>
-        <img src="path/to/your/image.jpg" alt="Description of image" />
-        <Typography>1.Website Strategy & Planning</Typography>
+    <Box className="our-process-component-container">
+      <Box className="our-process-item">
+        <img src="https://images.webfluid.studio/ourprocess-guy-1.svg" alt="Group hugging" />
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Strategy & Planning</Typography>
+        <Typography>We analyze your business goals and audience to develop a personalized strategic plan. Our aim is to ensure your website meets your needs and drives the desired results.</Typography>
+      </Box>
+      <Box className="our-process-item">
+        <img src="https://images.webfluid.studio/ourprocess-guy-2.svg" alt="Group hugging" />
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Strategy & Planning</Typography>
+        <Typography>We analyze your business goals and audience to develop a personalized strategic plan. Our aim is to ensure your website meets your needs and drives the desired results.</Typography>
+      </Box>
+      <Box className="our-process-item">
+        <img src="https://images.webfluid.studio/ourprocess-guy-3.svg" alt="Group hugging" />
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Strategy & Planning</Typography>
         <Typography>We analyze your business goals and audience to develop a personalized strategic plan. Our aim is to ensure your website meets your needs and drives the desired results.</Typography>
       </Box>
     </Box>
