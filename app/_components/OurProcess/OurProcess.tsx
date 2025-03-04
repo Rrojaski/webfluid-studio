168dbd8950bb983002af2e28d1358736caa1b678
Roman Rojas <rrojaski@gmail.com>
2025-03-04 15:03:48 -0500

    Update OurProcess component descriptions for clarity and accuracy

--- app/_components/OurProcess/OurProcess.tsx
+++ app/_components/OurProcess/OurProcess.tsx
@@ -12,13 +12,13 @@ function OurProcess() {
       </Box>
       <Box className="our-process-item">
         <img src="https://images.webfluid.studio/ourprocess-guy-2.svg" alt="Group hugging" />
-        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Strategy & Planning</Typography>
-        <Typography>We analyze your business goals and audience to develop a personalized strategic plan. Our aim is to ensure your website meets your needs and drives the desired results.</Typography>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Design & Development</Typography>
+        <Typography>We create professional, responsive, and high-performing websites. Using clean and optimized code, we deliver an engaging user experience focused on functionality and aesthetics.</Typography>
       </Box>
       <Box className="our-process-item">
         <img src="https://images.webfluid.studio/ourprocess-guy-3.svg" alt="Group hugging" />
-        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Strategy & Planning</Typography>
-        <Typography>We analyze your business goals and audience to develop a personalized strategic plan. Our aim is to ensure your website meets your needs and drives the desired results.</Typography>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Launch & Support</Typography>
+        <Typography>We ensure a hassle-free launch, making sure everything runs smoothly. Additionally, we provide training for your team and ongoing support to keep your website efficient, updated, and aligned with your goals.</Typography>
       </Box>
     </Box>
   );
