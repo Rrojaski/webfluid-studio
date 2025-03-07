50bb6da3f06db6d60a9736b087dc5e5097d60531
Roman Rojas <rrojaski@gmail.com>
2025-03-07 10:02:55 -0500

    Update OurProcess component step numbers for consistency

--- app/_components/OurProcess/OurProcess.tsx
+++ app/_components/OurProcess/OurProcess.tsx
@@ -12,12 +12,12 @@ function OurProcess() {
       </Box>
       <Box className="our-process-item">
         <img src="https://images.webfluid.studio/ourprocess-guy-2.svg" alt="Group hugging" />
-        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Design & Development</Typography>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>2.Website Design & Development</Typography>
         <Typography>We create professional, responsive, and high-performing websites. Using clean and optimized code, we deliver an engaging user experience focused on functionality and aesthetics.</Typography>
       </Box>
       <Box className="our-process-item">
         <img src="https://images.webfluid.studio/ourprocess-guy-3.svg" alt="Group hugging" />
-        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>1.Website Launch & Support</Typography>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>3.Website Launch & Support</Typography>
         <Typography>We ensure a hassle-free launch, making sure everything runs smoothly. Additionally, we provide training for your team and ongoing support to keep your website efficient, updated, and aligned with your goals.</Typography>
       </Box>
     </Box>
