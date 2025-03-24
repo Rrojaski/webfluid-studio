923ca9998744199879a0cada30d77f7282dbf8fb
Roman Rojas <rrojaski@gmail.com>
2025-03-24 19:51:11 -0400

    Fix typos in CallToAction3 component text and improve formatting

--- app/_components/CallToAction3/CallToAction3.tsx
+++ app/_components/CallToAction3/CallToAction3.tsx
@@ -6,11 +6,11 @@ function CallToAction3() {
     <Box className="call-to-action-3-wrapper">
       <Typography sx={{ typography: { md: "h2", sm: "h3", xs: "h4" } }}>Create better together.</Typography>
       <Typography>
-        Great ideas start with a simple conversation. We’re here to listen, collaborate, and turn your ideas into extraordinary results. Let’s create something
+        Great ideas start with a simple conversation. We're here to listen, collaborate, and turn your ideas into extraordinary results. Let's create something
         unique together!
       </Typography>
       <Button variant="outlined">Contact Us</Button>
-        <img className="call-to-action-3-image" src="https://images.webfluid.studio/getty-images-gEEj-X_03Cs-unsplash.svg" alt="Picture of the a group" />
+      <img className="call-to-action-3-image" src="https://images.webfluid.studio/getty-images-gEEj-X_03Cs-unsplash.svg" alt="Picture of the a group" />
     </Box>
   );
 }
