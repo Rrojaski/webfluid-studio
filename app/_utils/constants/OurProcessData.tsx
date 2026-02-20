c4c75eeacd845d3ce8feb6284f2015805059175e
Roman Rojas <rrojaski@gmail.com>
2026-02-20 15:04:31 -0500

    Update path

--- app/_utils/constants/OurProcessData.tsx
+++ app/_utils/constants/OurProcessData.tsx
@@ -8,32 +8,32 @@ const ourProccessData: OurProcessItemProps[] = [
   {
     step: 1,
     title: "Discovery & Onboarding",
-    image: "/assets/images/Discovery-Onboarding.png",
+    image: "/images/Discovery-Onboarding.png",
   },
   {
     step: 2,
     title: "Design",
-    image: "/assets/images/Design.png",
+    image: "/images/Design.png",
   },
   {
     step: 3,
     title: "Development",
-    image: "/assets/images/Development-pic.png",
+    image: "/images/Development-pic.png",
   },
   {
     step: 4,
     title: "Quality Assurance",
-    image: "/assets/images/Quality-Assurance.png",
+    image: "/images/Quality-Assurance.png",
   },
   {
     step: 5,
     title: "Launch & Training",
-    image: "/assets/images/Launch-Training.png",
+    image: "/images/Launch-Training.png",
   },
   {
     step: 6,
     title: "Post-Launch Support",
-    image: "/assets/images/Post-Launch-Support.png",
+    image: "/images/Post-Launch-Support.png",
   },
 ];
 
